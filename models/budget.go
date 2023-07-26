048bac80b3b1c16b4b2b910c019940e18f9ca9d9
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2023-07-26 16:04:36 +0800

    Update budget model db constraints

--- models/budget.go
+++ models/budget.go
@@ -3,12 +3,13 @@ package models
 import "time"
 
 type Budget struct {
+	gorm.Model
 	ID                uint      `json:"id" gorm:"primary_key"`
-	StartDate         time.Time `json:"startDate"`
-	EndDate           time.Time `json:"endDate"`
-	TotalBudgeted     float64   `json:"totalBudgeted"`
-	TotalAllocated    float64   `json:"totalAllocated"`
-	BalanceToAllocate float64   `json:"balanceToAllocate"`
-	CreatedAt         time.Time `json:"createdAt"`
-	UpdatedAt         time.Time `json:"updatedAt"`
+	StartDate         time.Time `json:"startDate" gorm:"not null"`
+	EndDate           time.Time `json:"endDate" gorm:"not null"`
+	TotalBudgeted     float64   `json:"totalBudgeted" gorm:"default:0;not null"`
+	TotalAllocated    float64   `json:"totalAllocated" gorm:"default:0;not null"`
+	BalanceToAllocate float64   `json:"balanceToAllocate" gorm:"default:0;not null"`
+	CreatedAt         time.Time `json:"createdAt" gorm:"not null"`
+	UpdatedAt         time.Time `json:"updatedAt" gorm:"not null"`
 }
